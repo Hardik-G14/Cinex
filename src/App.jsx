5dfec683d8feec91aefc62d02a8e1c878554ea2b
Hardik-G14 <hardikgathibandhe14@gmail.com>
2023-06-21 11:24:50 +0530

    setting routes done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 import { fetchDataFromApi } from './utils/api';
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './store/homeSlice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const { url } = useSelector((state) => state.home);
@@ -19,9 +28,17 @@ function App() {
     })
   }
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
