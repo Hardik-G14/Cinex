912353bde27db34f13590ba4c049dbeca417b21b
Hardik-G14 <hardikgathibandhe14@gmail.com>
2023-06-21 10:48:56 +0530

    reduxStore done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    })
+  }
   return (
     <div className="App">
       App
---
+++ src/pages/searchResult/SearchResult.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SearchResult.scss';
+
+const SearchResults = () => {
+    return (
+        <div>SearchResults</div>
+    )
+}
+
+export default SearchResults
